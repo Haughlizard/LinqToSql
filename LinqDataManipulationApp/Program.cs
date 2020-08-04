1e5bcbb67718a5e6c60e3156c88079493b02a3b5
生延 李 <lishengyan1991@qq.com>
2020-08-04 10:11:55 +0800

    添加联系项目

---
+++ LinqDataManipulationApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace LinqDataManipulationApp
+{
+    class Program
+    {
+        private const string CONNECT_STRING = @"Data Source=(localdb)\MSSQLLocalDB;
+                                                Initial Catalog=Northwind;Integrated Security=True;
+                                                Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;
+                                                ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        static void Main(string[] args)
+        {
+            // 使用连接字符串连接数据库
+            Northwnd db = new Northwnd(CONNECT_STRING);
+
+            Customer newCust = new Customer();
+            newCust.CompanyName = "AdventureWorks Cafe";
+            newCust.CustomerID = "ADVCA";
+
+            db.Customers.InsertOnSubmit(newCust);
+
+            Console.WriteLine("\nCustomers matching CA before insert");
+            foreach (var c in db.Customers.Where(cust => cust.CustomerID.Contains("CA")))
+            {
+                Console.WriteLine("{0}, {1}, {2}",
+                c.CustomerID, c.CompanyName, c.Orders.Count);
+            }
+
+            var existingCust =
+                (from c in db.Customers
+                 where c.CustomerID == "ALFKI"
+                 select c).First();
+            existingCust.ContactName = "New Contact";
+
+            Order order0 = existingCust.Orders[0];
+            OrderDetail detail0 = order0.OrderDetails[0];
+
+            // Display the order to be deleted.
+            Console.WriteLine("The Order Detail to be deleted is: OrderID = {0}, ProductID = {1}",
+            detail0.OrderID, detail0.ProductID);
+            // Mark the Order Detail row for deletion from the database.
+            db.OrderDetails.DeleteOnSubmit(detail0);
+
+            db.SubmitChanges();
+
+            Console.WriteLine("\nCustomers matching CA after update");
+            foreach (var c in db.Customers.Where(cust =>
+            cust.CustomerID.Contains("CA")))
+            {
+                Console.WriteLine("{0}, {1}, {2}",
+                c.CustomerID, c.CompanyName, c.Orders.Count);
+            }
+
+            // Keep the console window open after activity stops.
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SprocOnlyApp/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SprocOnlyApp
+{
+    public partial class Form1 : Form
+    {
+        private const string CONNECT_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;
+                                                AttachDbFilename=C:\linqtest7\northwnd.mdf;
+                                                Integrated Security=True;
+                                                Connect Timeout=30";
+
+        Northwnd db = new Northwnd(CONNECT_STRING);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string param = textBox1.Text;
+            var custQuery = db.CustOrdersDetail(Convert.ToInt32(param));
+            string msg = "";
+            foreach (CustOrdersDetailResult custOrdersDetail in custQuery)
+            {
+                msg = msg + custOrdersDetail.ProductName + "\n";
+            }
+            if(msg == "")
+                msg = "No results.";
+            MessageBox.Show(msg);
+            // Clear the variables before continuing.
+            param = "";
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Comments in the code for button2 are the same
+            // as for button1.
+            string param = textBox2.Text;
+            var custquery = db.CustOrderHist(param);
+            string msg = "";
+            foreach (CustOrderHistResult custOrdHist in custquery)
+            {
+                msg = msg + custOrdHist.ProductName + "\n";
+            }
+            MessageBox.Show(msg);
+            param = "";
+            textBox2.Text = "";
+        }
+    }
+}
